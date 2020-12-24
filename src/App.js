d5d5bc7cca93003bd4341f76ed4b65ebac0ac9a6
Celia <celiahrrferreira@gmail.com>
2020-12-24 12:56:38 +0100

    DEVELOP: Semantic-ui install first endpoints call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [fish, setFish] = useState([]);
+  const [bugs, setBugs] = useState([]);
+  const [fossils, setFossils] = useState([]);
+  const [villagers, setVillagers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchFish() {
+      let res = await fetch('https://acnhapi.com/v1/fish');
+      let data = await res.json();
+      setFish(data);
+    }
+    async function fetchBugs() {
+      let res = await fetch('https://acnhapi.com/v1/bugs');
+      let data = await res.json();
+      setBugs(data);
+    }
+    async function fetchFossils() {
+      let res = await fetch('https://acnhapi.com/v1/fossils');
+      let data = await res.json();
+      setFossils(data);
+    }
+    async function fetchVillagers() {
+      let res = await fetch('https://acnhapi.com/v1/villagers');
+      let data = await res.json();
+      setVillagers(data);
+    }
+
+    fetchFish();
+    fetchBugs();
+    fetchFossils();
+    fetchVillagers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hola!
     </div>
   );
 }
-
 export default App;
