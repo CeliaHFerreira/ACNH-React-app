c487a765cc0c79b4be3760048e38758ec25841ae
Celia <celiahrrferreira@gmail.com>
2020-12-27 14:23:20 +0100

    DEVELOP: Delete loading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Container, Dimmer, Loader } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import Home from './components/Home';
 import Fish from './components/Fish';
 import Bugs from './components/Bugs';
@@ -15,7 +15,6 @@ function App() {
   const [bugs, setBugs] = useState([]);
   const [artWork, setArtWork] = useState([]);
   const [villagers, setVillagers] = useState([]);
-  // const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchFish() {
@@ -26,7 +25,6 @@ function App() {
       });
       let data = await res.json();
       setFish(data);
-      // setLoading(true);
     }
     async function fetchBugs() {
       let res = await fetch('https://api.nookipedia.com/nh/bugs', {
@@ -36,7 +34,6 @@ function App() {
       });
       let data = await res.json();
       setBugs(data);
-      // setLoading(true);
     }
     async function fetchArtWork() {
       let res = await fetch('https://api.nookipedia.com/nh/art', {
@@ -46,24 +43,21 @@ function App() {
       });
       let data = await res.json();
       setArtWork(data);
-      // setLoading(true);
     }
     async function fetchVillagers() {
-      let res = await fetch('https://api.nookipedia.com/villagers?game=nh', {
+      let res = await fetch('https://api.nookipedia.com/villagers', {
         headers: {
           'x-api-key': '3c1ec54a-ee67-4b9d-8dbf-9c71004f0e97'
         }
       });
       let data = await res.json();
       setVillagers(data);
-      // setLoading(true);
     }
 
     fetchFish();
     fetchBugs();
     fetchArtWork();
     fetchVillagers();
-    // setLoading(false);
   }, []);
 
   return (
@@ -71,23 +65,23 @@ function App() {
       <Router>
         <NavBar />
         <Container>
-              <Switch>
-                <Route exact path='/'>
-                  <Home />
-                </Route>
-                <Route exact path='/fish'>
-                  <Fish data={fish}/>
-                </Route>
-                <Route exact path='/bugs'>
-                  <Bugs data={bugs}/>
-                </Route>
-                <Route exact path='/artwork'>
-                  <ArtWork data={artWork}/>
-                </Route>
-                <Route exact path='/villagers'>
-                  <Villagers data={villagers}/>
-                </Route>
-              </Switch>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/fish'>
+              <Fish data={fish} />
+            </Route>
+            <Route exact path='/bugs'>
+              <Bugs data={bugs} />
+            </Route>
+            <Route exact path='/artwork'>
+              <ArtWork data={artWork} />
+            </Route>
+            <Route exact path='/villagers'>
+              <Villagers data={villagers} />
+            </Route>
+          </Switch>
         </Container>
       </Router>
     </div>
