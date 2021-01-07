d8542a266ecde4ccb3b0ac260fd8c531e5eb4fd0
Celia <celiahrrferreira@gmail.com>
2021-01-07 13:52:37 +0100

    DEVELOP: Finnish styling home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './Home.css';
+import CheckDaily from './CheckDaily.js';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 import { Container } from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
+
+// images
 import fossil from '../../assets/images/fossil.png';
 import rock from '../../assets/images/rock.png';
 import tree from '../../assets/images/money-tree.png';
 import recipe from '../../assets/images/recipe-bottle.png';
 import flowers from '../../assets/images/watering-can.png';
 import residents from '../../assets/images/residents.png';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import CheckDaily from './CheckDaily.js';
-import { Icon } from 'semantic-ui-react';
+import turnip from '../../assets/images/turnip.png';
+import island from '../../assets/images/island.png';
+import ACNH from '../../assets/images/ACNH.png';
+import bells from '../../assets/images/bells.png';
+import calculator from '../../assets/images/calculator.png';
+import daisy from '../../assets/images/daisy-mae.png';
 
 
 class Home extends React.Component {
@@ -67,49 +76,97 @@ class Home extends React.Component {
             <Jumbotron className="jumbotron-home">
                 <h1 className="title-home">Animal Crossing New Horizons</h1>
                 <Container className="container-central">
-                    <h2>Daily Checklist</h2>
-                    <h4>Money, recipes, flowers and conversation</h4>
-                    <div className="daily">
-                        {
-                            this.state.daily.map((daily, index) => {
-                                return (<CheckDaily key={index} handleCheckChildElement={this.handleCheckChildElement} image={daily.image} {...daily} />)
-                            })
-                        }
-                    </div>
-                    <h4>Find fossils</h4>
-                    <div className="fossils">
-                        {
-                            this.state.fossils.map((fossil, index) => {
-                                return (<CheckDaily key={index} handleCheckChildElement={this.handleCheckChildElement} image={fossil.image} {...fossil} />)
-                            })
-                        }
-                    </div>
-                    <h4>Break rocks</h4>
-                    <div className="rocks">
-                        {
-                            this.state.rocks.map((rock, index) => {
-                                return (<CheckDaily key={index} handleCheckChildElement={this.handleCheckChildElement} image={rock.image} {...rock} />)
-                            })
-                        }
+                    <h2 className="title-list">
+                        <img alt="island" className="principal" src={island} />
+                        Daily Checklist
+                    </h2>
+                    <div className="stats">
+                        <h4>
+                            <img alt="icon" className="secondary" src={ACNH} />
+                            Money, recipes, flowers and conversation
+                        </h4>
+                        <div className="daily">
+                            {
+                                this.state.daily.map((daily, index) => {
+                                    return (<CheckDaily key={index} handleCheckChildElement={this.handleCheckChildElement} image={daily.image} {...daily} />)
+                                })
+                            }
+                        </div>
+                        <h4>
+                            <img alt="icon" className="secondary" src={ACNH} />
+                            Find fossils
+                        </h4>
+                        <div className="fossils">
+                            {
+                                this.state.fossils.map((fossil, index) => {
+                                    return (<CheckDaily key={index} handleCheckChildElement={this.handleCheckChildElement} image={fossil.image} {...fossil} />)
+                                })
+                            }
+                        </div>
+                        <h4>
+                            <img alt="icon" className="secondary" src={ACNH} />
+                            Hit rocks
+                        </h4>
+                        <div className="rocks">
+                            {
+                                this.state.rocks.map((rock, index) => {
+                                    return (<CheckDaily key={index} handleCheckChildElement={this.handleCheckChildElement} image={rock.image} {...rock} />)
+                                })
+                            }
+                        </div>
                     </div>
                 </Container>
                 <Container className="container-central">
-                    <h2>Turnip price</h2>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Prepend>
-                            <InputGroup.Text><Icon name='sun' size='large' /></InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <FormControl
-                            placeholder="Morning price"
-                        />
-                        <InputGroup.Prepend>
-                            <InputGroup.Text><Icon name='moon' size='large' /></InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <FormControl
-                            placeholder="Afternoon price"
-                        />
-                    </InputGroup>
-                    <h4>Stats of turnip price</h4>
+                    <h2 className="title-list">
+                        <img alt="turnip" className="principal" src={turnip} />
+                        Turnips
+                    </h2>
+                    <div className="stats">
+                        <h3>
+                            <img alt="daisy-mae" className="principal" src={daisy} />
+                            Turnip price
+                    </h3>
+                        <h4 className="buy-sell">
+                            <img alt="bells" className="secondary" src={bells} />
+                            Purchase price
+                        </h4>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Prepend>
+                                <InputGroup.Text><Icon name='star' size='large' /></InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl
+                                placeholder="Purchase price sunday"
+                                aria-label="Purchase price sunday"
+                            />
+                            <InputGroup.Append>
+                                <Button variant="success">Save</Button>
+                            </InputGroup.Append>
+                        </InputGroup>
+                        <h4 className="buy-sell">
+                            <img alt="bells" className="secondary" src={bells} />
+                            Selling price
+                        </h4>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Prepend>
+                                <InputGroup.Text><Icon name='sun' size='large' /></InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl
+                                placeholder="Morning price"
+                            />
+                            <InputGroup.Prepend>
+                                <InputGroup.Text><Icon name='moon' size='large' /></InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl
+                                placeholder="Afternoon price"
+                            />
+                        <Button className="button-sell" variant="success" size="lg" block>Save</Button>
+                        </InputGroup>
+                        <h4 className="prices">
+                            <img alt="calculator" className="secondary" src={calculator} />
+                            Stats of turnip price
+                        </h4>
+                        {/* falta la tabla */}
+                    </div>
                 </Container>
             </Jumbotron>
 
